df3b848c054980e8988c7cfd0526adb298538fd7
Wim Vodderie <wim.vodderie@hotmail.com>
2019-01-23 20:39:16 +0100

    Use black formatter

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class HistoryForm(FlaskForm):
-    servername = StringField('Servername', validators=[DataRequired()])
-    submit = SubmitField('Display')
+    servername = StringField("Servername", validators=[DataRequired()])
+    submit = SubmitField("Display")
